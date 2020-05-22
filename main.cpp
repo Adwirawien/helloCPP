5138c7332c3c3be80baf0b5e9f77aac37f09c0f6
Adrian <hannsadrian@gmail.com>
2020-05-22 22:18:23 +0200

    Update main file

--- main.cpp
+++ main.cpp
@@ -1,27 +1,10 @@
-// this is a comment
-
-/*
- * With multi line owo
- */
+//
+// Created by Adrian Böhme on 22.05.20.
+//
 
 #include <iostream>
-#include <vector>
-#include <string>
-#include <fstream>
 
 int main() {
-    std::cout << "Hello World!" << std::endl;
-
-    const double PI = 3.1415926536;
-
-    char myGrade = 'A';
-    bool isHappy = true;
-    int myAge = 15;
-    float favNum = 3.141592;
-
-    std::cout << "Favourite Number is " << favNum << ". Age is " << myAge << "." << std::endl;
-
-    std::cout << "Size of int " << sizeof(myAge) << std::endl;
 
     return 0;
 }
