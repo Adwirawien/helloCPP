c0f20a067966a7a8cbabb4964d19b9ec736f4c5f
Adrian <hannsadrian@gmail.com>
2020-05-22 21:41:05 +0200

    Add basic data types

--- main.cpp
+++ main.cpp
@@ -1,6 +1,27 @@
+// this is a comment
+
+/*
+ * With multi line owo
+ */
+
 #include <iostream>
+#include <vector>
+#include <string>
+#include <fstream>
 
 int main() {
-    std::cout << "Hello, World!" << std::endl;
+    std::cout << "Hello World!" << std::endl;
+
+    const double PI = 3.1415926536;
+
+    char myGrade = 'A';
+    bool isHappy = true;
+    int myAge = 15;
+    float favNum = 3.141592;
+
+    std::cout << "Favourite Number is " << favNum << ". Age is " << myAge << "." << std::endl;
+
+    std::cout << "Size of int " << sizeof(myAge) << std::endl;
+
     return 0;
 }
